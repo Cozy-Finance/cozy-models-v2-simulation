312e7d9052f85d9c732a841209b4ff0ae326f908
Dendrimer <franzjchen@gmail.com>
2022-10-06 12:23:31 -0400

    chore: bump solc version to 0.8.16

--- src/CostModelJumpRate.sol
+++ src/CostModelJumpRate.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
-pragma solidity 0.8.15;
+pragma solidity 0.8.16;
 
 import "solmate/utils/FixedPointMathLib.sol";
 import "cozy-v2-interfaces/interfaces/ICostModel.sol";
--- src/lib/Create2.sol
+++ src/lib/Create2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
-pragma solidity 0.8.15;
+pragma solidity 0.8.16;
 
 library Create2 {
   /// @notice Computes the address that would result from a CREATE2 call for a contract according
