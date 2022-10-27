2477b6e83a336c6d3a74c6aac8c7e19284915a77
Matt Solomon <matt@mattsolomon.dev>
2022-10-27 08:13:06 -0700

    chore: update submodule and comments

--- script/DeployDripDecayModelConstant.s.sol
+++ script/DeployDripDecayModelConstant.s.sol
@@ -39,14 +39,19 @@ contract DeployDripDecayModelConstant is ScriptUtils {
   // -------- Configured Inputs --------
   // -----------------------------------
 
-  // For calculating the per-second decay rate, we use the exponential decay formula A = P * (1 - r) ^ t
-  // where A is final amount, P is principal (starting) amount, r is the per-second decay rate, and t is the number of elapsed seconds.
-  // For example, for an annual decay rate of 25%:
-  // A = P * (1 - r) ^ t
-  // 0.75 = 1 * (1 - r) ^ 31557600
-  // -r = 0.75^(1/31557600) - 1
-  // -r = -9.116094732822280932149636651070655494101566187385032e-9
-  // Multiplying r by -1e18 to calculate the scaled up per-second value required by decay model constructors ~= 9116094774
+  // For calculating the per-second rate, we use the exponential decay formula:
+  //   A = P * (1 - r) ^ t
+  // where:
+  //   A is final amount.
+  //   P is principal (starting) amount.
+  //   r is the per-second drip/decay rate.
+  //   t is the number of elapsed seconds.
+  // For example, for an annual drip/decay rate of 25%:
+  //   A = P * (1 - r) ^ t
+  //   0.75 = 1 * (1 - r) ^ 31557600
+  //   -r = 0.75^(1/31557600) - 1
+  //   -r = -9.116094732822280932149636651070655494101566187385032e-9
+  // Multiplying r by -1e18 to calculate the scaled up per-second value required by the constructor ~= 9116094774
   uint256 dripDecayRatePerSecond;
 
   DripDecayModelConstantFactory factory;
